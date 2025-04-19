b891165d0ad927874701723df4345b4114cfe11c
Naveen gupta <gjagarnath54@gmail.com>
2025-04-19 22:02:27 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ const [formLoginData, setLoginFormData] = useState({
     }
 
     try {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("https://react-backend-api.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
