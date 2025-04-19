1e9daf5c5ed8cce1d1c351a1ebaf5c0b76047662
Naveen gupta <gjagarnath54@gmail.com>
2025-04-19 22:11:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/About'
 import User from './components/User'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 import { createBrowserRouter , RouterProvider } from 'react-router-dom'
 import Login from './components/Login'
